b09f8e8828a9e2fae8e536e2ec001b51b03c8ec0
Manuel Hieke <mahieke@htwg-konstanz.de>
2014-10-07 16:10:04 +0200

    first text for README.mk

---
+++ README.mk
@@ -0,0 +1,46 @@
+# FLASH_TOOL
+
+...Describtion...
+
+
+## Supported Platforms
+
+This configuration affects kernel, flatten-device-tree files and u-boot.
+
+ - Colibri T20 
+   - Iris Board
+   - ARMrider
+
+ - Raspberry Pi
+ 
+ - Beaglebone (in process)
+
+
+## Supported Labs
+
+This configuration affects rootFS and config partition (see below).
+
+ - BASE (light-weight rootFS)
+ - SYSO (Systemsoftware)
+ - BSYS (Betriebssysteme)
+
+You can find a list of all contained software packages in
+chapter Software Packages.
+
+
+## Software Packages
+
+Lists with packages per rootFS configuration:
+
+_BASE:_
+ - ...
+
+
+_SYSO:_
+ - ...
+
+
+_BSYS:_
+ - ...
+
+
