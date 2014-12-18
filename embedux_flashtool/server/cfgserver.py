53c738231b26b2d1cbd959b8370c1de2c0f76c1d
Manuel Hieke <mahieke@htwg-konstanz.de>
2014-12-18 17:40:04 +0100

    Configserver wrapper

---
+++ embedux_flashtool/server/cfgserver.py
@@ -0,0 +1,21 @@
+__author__ = 'mahieke'
+
+from embedux_flashtool.server import Git
+
+class ConfigServer():
+    '''
+    Gets all files from a given Git repository
+    '''
+
+    def __init__(self, url, dest, name):
+        self.__url = url
+        self.__dest = dest
+        self.__accessor = Git(url, dest, name)
+
+
+    def get_initial(self):
+        self.__accessor.initial_load()
+
+
+    def update_confs(self):
+        self.__accessor.update()
